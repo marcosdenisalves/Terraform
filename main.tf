43c495da482a2b1c2fc0fe234bce272f808fa2d2
marcosdenisalves <dee.alves2@gmail.com>
2023-01-29 17:56:53 -0300

    Improving provisioning with ansible

--- main.tf
+++ main.tf
@@ -1,26 +1,25 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.27"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
+  }
 
-    required_version = ">= 0.14.9"
+  required_version = ">= 0.14.9"
 }
 
-provider "aws" {    
-  region  = "us-west-2"
+provider "aws" {
+  region     = "us-east-1"
   access_key = ""
   secret_key = ""
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-095413544ce52437d"
+  ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
-  key_name = "iac-alura"
-
+  key_name      = "iac-study"
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste aws"
   }
 }
